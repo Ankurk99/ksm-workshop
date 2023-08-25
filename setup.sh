8e88dc4ec8a55907b1175aa37bb4d278b8a473dc
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2023-08-26 02:59:51 +0530

    add policies + installation for dvwa
    Signed-off-by: Ankur Kothiwal <ankur.kothiwal99@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# Clone the repo
+git clone https://github.com/Ankurk99/ksm-workshop.git
+
 # Install k3s
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
 KUBEDIR=$HOME/.kube
@@ -16,7 +19,18 @@ kubectl wait --for=condition=Ready node --all --timeout=300s
 # Install KubeArmor
 helm repo add kubearmor https://kubearmor.github.io/charts
 helm repo update kubearmor
-helm upgrade --install kubearmor-operator kubearmor/kubearmor-operator -n kubearmor --create-namespace
+helm upgrade --install kubearmor-operator kubearmor/kubearmor-operator -n kube-system
+kubectl apply -f https://raw.githubusercontent.com/kubearmor/KubeArmor/main/pkg/KubeArmorOperator/config/samples/sample-config.yml
+
+# Install Discovery engine
+kubectl apply -f https://raw.githubusercontent.com/accuknox/discovery-engine/dev/deployments/k8s/deployment.yaml
+
+# Install Wordpress workload
+kubectl apply -f https://raw.githubusercontent.com/kubearmor/KubeArmor/main/examples/wordpress-mysql/wordpress-mysql-deployment.yaml
 
 # Install karmor-cli
 curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin
+sleep 1
+karmor probe
+
+
